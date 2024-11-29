9cac10eca3619da2079c8b5e6649e13fc6bd89ab
Stanley Omondi <stanleyomondi@Stanleys-MacBook-Pro-2.local>
2024-11-29 13:22:07 -0500

    Worked on curse, retrty running with the diff dimensions

--- hw4/curse.py
+++ hw4/curse.py
@@ -9,26 +9,44 @@ line_styles = {0: 'ro-', 1: 'b^-', 2: 'gs-', 3: 'cv-'}
 
 # Plot dmax/dmin ratio
 # TODO: fill in valid test numbers
-for idx, num_samples in enumerate([]):
+sample_size = [100, 200, 300, 400]
+for idx, num_samples in enumerate(sample_size):
     # TODO: Fill in a valid feature range
-    feature_range = None
+    feature_range = range(1,101)
     ratios = []
     for num_features in feature_range:
         # TODO: Generate synthetic data using make_classification
-        X = None
-        
+        num_info = max(1, num_features // 2 ) #flooring to get integer, n_informative on takes integers
+        numOfClasses = min(2, max(2, num_features / 10))
+        X,Y  = make_classification(
+            n_samples= num_samples,
+            n_features= num_features,
+            n_informative= num_info,
+            n_classes= numOfClasses,
+            n_clusters_per_class=1,
+            n_redundant=0,
+            n_repeated=0,
+            random_state=0
+        )
+        #print(X)
         # TODO: Choose random query point from X
-        query_point = None 
+        query_pointIdx = np.random.randint(0, X.shape[0])
+        query_point = X[query_pointIdx]
         
         # TODO: remove query pt from X so it isn't used in distance calculations
+        querypoint = np.delete(X, query_pointIdx, axis=0)
 
         # TODO: Calculate distances
-        distances = None
-        ratio = np.max(distances) / np.min(distances)
+        distances = np.linalg.norm(querypoint - query_point, axis=1)
+        if np.min(distances) == 0:
+            ratio = float('inf')
+        else:
+            ratio = np.max(distances) / np.min(distances)
         ratios.append(ratio)
-
+    
     ax.plot(feature_range, np.log(ratios), line_styles[idx], label=f'N={num_samples:,}')
-
+    
 plt.legend()
 plt.tight_layout()
 plt.grid(True)
+plt.show()
